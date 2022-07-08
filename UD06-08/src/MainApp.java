11df19f965484aa0aa7837d529375f3a94dcd6fe
Jimxx22 <jaumegil22@gmail.com>
2022-07-08 13:06:29 +0200

    Ejercicio 8

---
+++ UD06-08/src/MainApp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		 int[] array = rellenarArray();
+		 mostrarArray(array);
+	}
+	
+	public static int[] rellenarArray() {
+		int[] array = new int[10];
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Vamos a rellenar la array.");
+		
+		for(int i=0;i<10;i++) {
+			System.out.print("Introduce un numero: ");
+			//Si se introduce otra cosa que no sea un numero entero salta error.
+			array[i]=sc.nextInt();	
+		}
+		System.out.println("La array ya esta llena.");
+		
+		return array;
+	}
+
+	public static void mostrarArray(int[] array) {
+		
+		System.out.println("\nVamos a mostrar la array.");
+		
+		for(int i=0;i<array.length;i++) {
+			System.out.println("En la posicion "+i+" encontramos el valor: "+array[i]);
+		}
+		
+	}
+}
