54353bc29cc8a3b32e181c0a0d5a6f7044fdd06c
Jimxx22 <jaumegil22@gmail.com>
2022-07-08 13:04:42 +0200

    Ejercicio 4

---
+++ UD06-04/src/MainApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner (System.in);
+		int n;
+		
+		System.out.print("Introduce un numero para calcular el factorial de este: ");
+		n=sc.nextInt();
+		
+		System.out.println("El factorial de "+n+" es: "+factorial(n));
+
+	}
+	
+	public static int factorial(int n) {
+		
+		int con=1;
+		
+		for(int i=1;i<=n;i++) {
+			con*=i;
+		}
+		return con;
+	}
+
+}
